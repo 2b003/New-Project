ce97e41736260eeeadc07f4481dc1fd206f509fd
2b003 <mukarotatenda4@gmail.com>
2025-10-27 23:39:31 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
 const app = express();
 const port = 3000 ; 
 
-let jobs = 
+let jobs = [
+{id : 1 ,title: "Software Engineer",company:"TechCorp", location: "Develop innovative software solutions ."},
+{id : 2 , title: "Data Analyst", company: "DataInc", location:"South Africa", description:"Analyze data to drive business insights."}
+];
+app.use(json());
+app.use(statistics ('public'));
+
+app.get('/api/jobs',(req,res) =>{
+res.json(jobs);
+});
+
+app.listen(port,()=>{
+console.log(`server running at http://localhost:${port}`);
+
+
+});
